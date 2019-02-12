2134cc88b786bc048e08301239e6eb6044b0b1f5
Julián Fernández Reyes <jfernandezreyes@anac.gob.ar>
2019-02-12 12:44:48 -0300

    #3 Schemas & Resolvers

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const app = require("express")();
+const { buildSchema } = require("graphql");
 
 app.use(require("body-parser").json());
 
-app.get("/", (req, res, next) => res.status(200).send("Olo"));
+app.use("/graphql", require("express-graphql")({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+  
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ["Romantic Cooking", "Sailing", "All-Night Coding"];
+    },
+    createEvent: (args) => {
+      return args.name;
+    }
+  },
+  graphiql: true,
+}));
 
 app.listen(3001);
