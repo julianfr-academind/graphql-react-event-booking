7ce60908f1d7339e3ad5a3fd0fba88c6996a1658
Julián Fernández Reyes <jfernandezreyes@anac.gob.ar>
2019-04-03 12:42:22 -0300

    #8 Adding Bookings

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 
 const Event = require("../../models/event");
 const User = require("../../models/user");
+const Booking = require("../../models/booking");
 
 const findUser = async user => {
   try {
@@ -30,6 +31,20 @@ const findEvents = async events => {
   }
 };
 
+const findEvent = async event => {
+  try {
+    const foundEvent = await Event.findById(event);
+
+    return {
+      ...foundEvent._doc,
+      date: new Date(foundEvent._doc.date).toISOString(),
+      user: findUser(foundEvent._doc.user),
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   events: async () => {
     try {
@@ -45,17 +60,37 @@ module.exports = {
     }
   },
 
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find();
+
+      if (bookings) {
+        return bookings.map(booking => ({
+          ...booking._doc,
+          event: findEvent.bind(this, booking.event),
+          user: findUser.bind(this, booking.user),
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString(),
+        }));
+      } else {
+        return new Error("Bookings not found!")
+      }
+    } catch (error) {
+      throw error;
+    }
+  },
+
   createEvent: async args => {
     const event = new Event({
       title: args.event.title,
       description: args.event.description,
       price: +args.event.price,
       date: new Date().toISOString(),
-      user: "5c656eb62b75bd2650daa55b",
+      user: "5c6d63af8367d62d6c62deb1",
     });
 
     try {
-      const user = await User.findById("5c656eb62b75bd2650daa55b");
+      const user = await User.findById("5c6d63af8367d62d6c62deb1");
 
       if (!user) throw new Error("User doesn't exists.");
 
@@ -88,4 +123,49 @@ module.exports = {
       throw err;
     }
   },
+
+  createBooking: async args => {
+    try {
+      const event = await Event.findById(args.event);
+
+      if (event) {
+        const booking = new Booking({
+          user: "5c6d63af8367d62d6c62deb1",
+          event,
+        });
+
+        const createdBooking = await booking.save();
+
+        return {
+          ...createdBooking._doc,
+          event,
+          user: findUser.bind(this, booking.user),
+          createdAt: new Date(createdBooking._doc.createdAt).toISOString(),
+          updatedAt: new Date(createdBooking._doc.updatedAt).toISOString(),
+        };
+      }
+      else {
+        throw new Error("Event not found!");
+      }
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  deleteBooking: async args => {
+    try {
+      const booking = await Booking.findById(args.booking).populate("event");
+
+      if (!booking) return new Error("Booking not found!");
+
+      const event = { ...booking.event._doc };
+      console.log(event);
+
+      await Booking.deleteOne({ _id: args.booking });
+
+      return { ...event, user: findUser.bind(this, event.user) };
+    } catch (error) {
+      throw error;
+    }
+  }
 }
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,44 +1,55 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
-    type Event {
-      _id: ID!
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-      user: User!
-    }
+  type Booking {
+    _id: ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAt: String!
+  }
 
-    type User {
-      _id: ID!
-      email: String!
-      password: String
-      events: [Event!]
-    }
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+    user: User!
+  }
 
-    input EventInput {
-      title: String!
-      description: String!
-      price: Float!
-    }
+  type User {
+    _id: ID!
+    email: String!
+    password: String
+    events: [Event!]
+  }
 
-    input UserInput {
-      email: String!
-      password: String!
-    }
-  
-    type RootQuery {
-      events: [Event!]!
-    }
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+  }
 
-    type RootMutation {
-      createEvent(event: EventInput): Event
-      createUser(user: UserInput): User
-    }
-  
-    schema {
-      query: RootQuery
-      mutation: RootMutation
-    }
+  input UserInput {
+    email: String!
+    password: String!
+  }
+
+  type RootQuery {
+    events: [Event!]!
+    bookings: [Booking!]!
+  }
+
+  type RootMutation {
+    createEvent(event: EventInput): Event
+    createUser(user: UserInput): User
+    createBooking(event: ID!): Booking!
+    deleteBooking(booking: ID!): Event!
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
   `)
