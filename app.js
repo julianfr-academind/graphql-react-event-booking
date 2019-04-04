9ff3080925a1e4b7d959d780b87ced1bf54bf3d5
Julián Fernández Reyes <jfernandezreyes@anac.gob.ar>
2019-04-04 16:00:33 -0300

    #13 Hitting the API

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const app = require("express")();
 
 app.use(require("body-parser").json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(require("./middleware/authenticate"));
 
-app.use("/graphql", require("express-graphql")({
-  schema: require("./graphql/schema"),
-  rootValue: require("./graphql/resolvers"),
-  graphiql: true,
-}));
+app.use(
+  "/graphql",
+  require("express-graphql")({
+    schema: require("./graphql/schema"),
+    rootValue: require("./graphql/resolvers"),
+    graphiql: true
+  })
+);
 
 require("mongoose")
   .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@julianfr-academind-node-shop-large.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
   .then(() => app.listen(3001))
-  .catch(error => console.log(error))
+  .catch(error => console.log(error));
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,9 +1,89 @@
 import React, { Component } from "react";
+import "./Auth.css";
 
 class AuthPage extends Component {
-  render() {
-    return (<h1>The Auth Page</h1>);
+  state = {
+    isLogin: true
+  };
+
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
   }
+
+  switchHandler = () => {
+    this.setState(prevState => {
+      return { isLogin: !prevState.isLogin };
+    });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) return;
+
+    let query = this.state.isLogin
+      ? {
+          query: `
+      query {
+        login(email:"${email}", password:"${password}") {
+          user
+          token
+          expiration
+        }
+      }
+      `
+        }
+      : {
+          query: `
+          mutation {
+            createUser(user:{ email:"${email}", password:"${password}"}) {
+              _id
+              email
+            }
+          }
+        `
+        };
+
+    fetch("http://localhost:3001/graphql", {
+      method: "POST",
+      body: JSON.stringify(query),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
+
+  render = () => (
+    <form className="auth-form" onSubmit={this.submitHandler}>
+      <div className="form-control">
+        <label htmlFor="email">E-Mail</label>
+        <input type="email" id="email" ref={this.emailEl} />
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" ref={this.passwordEl} />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+        <button type="button" onClick={this.switchHandler}>
+          Switch to {this.state.isLogin ? "Signup" : "Login"}
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default AuthPage;
